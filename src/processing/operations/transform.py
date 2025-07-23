[FIX] Remove truncate normalization from default pipeline

src/processing/operations/transform.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from .normalize import clahe, truncate_normalization
+from .normalize import clahe
 
 
 def crop_to_roi(image: np.array):
@@ -24,12 +24,10 @@ def crop_to_roi(image: np.array):
     )
     cnt = max(cnts, key=cv2.contourArea)
     x, y, w, h = cv2.boundingRect(cnt)
-    
-    norm_image = truncate_normalization((original_image[y: y + h, x: x + w], breast_mask[y: y + h, x: x + w]))
-    return norm_image
+    return original_image[y: y + h, x: x + w]
 
 
-def resize(image: np.array, new_size=2012):
+def resize(image: np.array, new_size=1024):
     try:
         return cv2.resize(
             image,